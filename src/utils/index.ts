5ce588f3f3d50781502280ebd62345a7af61b3a9
JimiC <jimikar@gmail.com>
2017-11-29 15:36:49 +0200

    Add ability to automatically find the supported files and folders file.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as path from 'path';
 
 export function pathUnixJoin(...paths: string[]): string {
@@ -7,4 +8,41 @@ export function pathUnixJoin(...paths: string[]): string {
 export function combine(array1: any[], array2: any[], separator = '.'): any[] {
   return array1.reduce((previous: string[], current: string) =>
     previous.concat(array2.map(value => [current, value].join(separator))), []);
+}
+
+export function findDirectorySync(dirName: string): string {
+  let dir = path.resolve();
+  const root = path.parse(dir).root;
+  while (true) {
+    let lookUpDir: string;
+    try {
+      fs.accessSync(path.resolve(dir, dirName));
+      lookUpDir = dirName;
+    } catch (err) {
+      lookUpDir = undefined;
+    }
+    if (lookUpDir) {
+      return path.join(dir, lookUpDir);
+    } else if (dir === root) {
+      return null;
+    }
+    dir = path.dirname(dir);
+  }
+}
+
+export function findFileSync(filePath: string | RegExp, rootPath?: string, results?: string[]): string[] {
+  if (!!!rootPath) { rootPath = path.resolve(); }
+  if (!!!results) { results = []; }
+  const files = fs.readdirSync(rootPath);
+  for (const file of files) {
+    const filename = path.join(rootPath, file);
+    const stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) { findFileSync(filePath, filename, results); }
+    if (filePath instanceof RegExp) {
+      if (filePath.test(filename)) { results.push(filename); }
+      continue;
+    }
+    if (filename.indexOf(filePath) > -1) { results.push(filename); }
+  }
+  return results;
 }
--- src/wpilgenerator.ts
+++ src/wpilgenerator.ts
@@ -1,7 +1,7 @@
 
 import { Logger } from './logger';
 import { YargsParser } from './yargsParser';
-import { pathUnixJoin } from './utils';
+import { findDirectorySync, findFileSync } from './utils';
 import { IResult } from './interfaces';
 import { GitClient } from './git-client';
 import { FilesListGenerator } from './filesListGenerator';
@@ -9,16 +9,19 @@ import { FoldersListGenerator } from './foldersListGenerator';
 
 export async function main(): Promise<void> {
   const logger = new Logger();
-  const pargs = new YargsParser(logger).parse();
-  const gitClient = new GitClient(pargs, logger);
 
   try {
-    // This directory should point to the 'vscode-icons' root directory
-    // TODO: Locate 'vscode-icons manifest folder'
-    const dirname = './../../../';
-    const manifestFolder = 'out/src/icon-manifest/';
-    const files = require(pathUnixJoin(dirname, manifestFolder, 'supportedExtensions')).extensions;
-    const folders = require(pathUnixJoin(dirname, manifestFolder, 'supportedFolders')).extensions;
+    const pargs = new YargsParser(logger).parse();
+    const gitClient = new GitClient(pargs, logger);
+
+    // Locate 'vscode-icons' root directory
+    const rootDir = findDirectorySync('vscode-icons');
+
+    // Find files and folders path
+    const filesPath = findFileSync(new RegExp('src/.*/supportedExtensions.js', 'g'), rootDir);
+    const foldersPath = findFileSync(new RegExp('src/.*/supportedFolders.js', 'g'), rootDir);
+    const files = require(filesPath[0]).extensions;
+    const folders = require(foldersPath[0]).extensions;
 
     // clone or open repo
     await Promise.all([gitClient.getCodeRepository(), gitClient.getWikiRepository()]);
