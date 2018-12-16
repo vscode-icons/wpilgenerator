972442c7955b290ffca5047db3277eec5ce2bef5
JimiC <jimikar@gmail.com>
2018-12-16 16:14:49 +0200

    Fix search pattern regex

--- src/wpilgenerator.ts
+++ src/wpilgenerator.ts
@@ -18,10 +18,11 @@ export async function main(): Promise<void> {
     const rootDir = findDirectorySync('vscode-icons');
 
     // Find files and folders path
-    const filesPath = findFileSync(/src((\/|\\)[a-z0-9\s_@\-^!#$%&+={}\[\]]+)*(\/|\\)supportedExtensions\.js/,
-     rootDir)[0];
-    const foldersPath = findFileSync(/src((\/|\\)[a-z0-9\s_@\-^!#$%&+={}\[\]]+)*(\/|\\)supportedFolders\.js/,
-     rootDir)[0];
+    const baseRegex = 'src(?:(?:\\/|\\\\)[a-zA-Z0-9\\s_@\-^!#$%&+={}\\[\\]]+)*(?:\\/|\\\\)';
+    const extensionsRegex = new RegExp(`${baseRegex}supportedExtensions\\.js`);
+    const filesPath = findFileSync(extensionsRegex, rootDir)[0];
+    const foldersRegex = new RegExp(`${baseRegex}supportedExtensions\\.js`);
+    const foldersPath = findFileSync(foldersRegex, rootDir)[0];
     const files = require(filesPath).extensions;
     const folders = require(foldersPath).extensions;
 
